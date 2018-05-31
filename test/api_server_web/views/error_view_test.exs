c2dc03f7b150ce66a434c3a6363cd4ad751f7210
Viet <g-viet@eole.co.jp>
2018-05-31 18:43:47 +0900

    First commit

---
+++ test/api_server_web/views/error_view_test.exs
@@ -0,0 +1,16 @@
+defmodule ApiServerWeb.ErrorViewTest do
+  use ApiServerWeb.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders 404.html" do
+    assert render_to_string(ApiServerWeb.ErrorView, "404.html", []) ==
+           "Not Found"
+  end
+
+  test "renders 500.html" do
+    assert render_to_string(ApiServerWeb.ErrorView, "500.html", []) ==
+           "Internal Server Error"
+  end
+end
